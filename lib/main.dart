f23ff5c9359cd048e60156dc1c0d7e3bff7b525d
Marcelo Iepsen <marcelo.iepsen@accedo.tv>
2019-10-19 09:07:12 +0200

    Changed paths to use models and services from the new structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-import 'dart:convert';
 import 'dart:math';
-import 'package:fluttertv/photo.dart';
-import 'package:http/http.dart' as http;
+import 'package:fluttertv/models/photo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'focus_item.dart';
+import 'services/fetch_photos.dart';
 
 void main() => runApp(MyApp());
 
@@ -42,16 +41,6 @@ class HomePageState extends State<MyHomePage> {
     photos = fetchPhotos();
   }
 
-  Future<List<Photo>> fetchPhotos() async {
-    final response = await http.get('https://picsum.photos/v2/list');
-
-    if (response.statusCode == 200) {
-      Iterable list = json.decode(response.body);
-      return list.map((model) => Photo.fromJson(model)).toList();
-    } else {
-      throw Exception('Failed to load photos');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
