5b79776380ac26808d1244917af2368eefa89425
Marcelo Iepsen <marcelo.iepsen@accedo.tv>
2019-10-19 09:06:46 +0200

    Added services layer

---
+++ lib/services/fetch_photos.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:fluttertv/models/photo.dart';
+
+Future<List<Photo>> fetchPhotos() async {
+  final response = await http.get('https://picsum.photos/v2/list');
+
+  if (response.statusCode == 200) {
+    Iterable list = json.decode(response.body);
+    return list.map((model) => Photo.fromJson(model)).toList();
+  } else {
+    throw Exception('Failed to load photos');
+  }
+}
